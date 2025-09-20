b3881d78011a8ebd874d8cd076125eb31187fd3e
jayanth8423 <163724366+jayanth8423@users.noreply.github.com>
2025-09-20 09:44:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/grosery/',  // 🔥 VERY IMPORTANT for subdirectory deployment
+  base: './',  // 🔥 VERY IMPORTANT for subdirectory deployment
   build: {
     outDir: 'dist', // optional; 'dist' is the default
   },
