09d626f63fdb69618a1bb7c43bc5393c90ab4a7e
jayanth8423 <163724366+jayanth8423@users.noreply.github.com>
2025-09-20 09:45:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import CustomerHome from "./CustomerHome";
 
 function App() {
   return (
-    <Router basename="/gro">
+    <Router basename="./">
       <Routes>
         {/* Default route → redirect to CustomerHome */}
         <Route path="/" element={<Navigate to="/CustomerHome" replace />} />
