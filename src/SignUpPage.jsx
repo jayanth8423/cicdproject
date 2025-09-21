4a38250540cdd46960d2cab3e0e5f38ff53b4759
jayanth8423 <163724366+jayanth8423@users.noreply.github.com>
2025-09-21 15:39:57 +0530

    Update SignUpPage.jsx

--- src/SignUpPage.jsx
+++ src/SignUpPage.jsx
@@ -23,7 +23,7 @@ export default function Signup() {
             return;
         }
 
-        axios.post("http://localhost:8080/api/signup", {
+        axios.post("http://localhost:8081/api/signup", {
             username,
             email,
             password,
